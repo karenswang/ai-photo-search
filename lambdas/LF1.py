2a56f1343ff93acac5c9fabd36c0e321f45cc2a8
Karen Wang <46947530+karenswang@users.noreply.github.com>
2023-11-11 13:29:05 -0500

    add cfn template and lambda functions

---
+++ lambdas/LF1.py
@@ -0,0 +1,99 @@
+import boto3
+import json
+import os
+from datetime import datetime
+from botocore.exceptions import ClientError
+
+
+def lambda_handler(event, context):
+    rekognition = boto3.client('rekognition')
+    s3 = boto3.client('s3')
+    opensearch = boto3.client('opensearchservice') # es = boto3.client('es')
+    
+    # Get bucket name and object key from the S3 event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
+
+    # Detect labels using Rekognition
+    response = rekognition.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': object_key
+            }
+        }
+        # MaxLabels=10
+    )
+
+    # Extract labels
+    labels = [label['Name'] for label in response['Labels']]
+
+    # Get custom labels from S3 metadata if available
+    metadata = s3.head_object(Bucket=bucket, Key=object_key)
+    custom_labels = metadata.get('Metadata', {}).get('x-amz-meta-customlabels', '')
+    if custom_labels:
+        labels.extend(custom_labels.split(','))
+
+    # Prepare the document to be indexed in OpenSearch
+    document = {
+        'objectKey': object_key,
+        'bucket': bucket,
+        'createdTimestamp': datetime.now().isoformat(),
+        'labels': labels
+    }
+
+    # Index the document in OpenSearch
+    index_response = opensearch.index(
+        Index='photos',
+        Body=json.dumps(document)
+    )
+    # opensearch.index(index='photos', doc_type='_doc', body=document)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(index_response)
+    }
+
+# def lambda_handler(event, context):
+#     for record in event['Records']:
+#         bucket_name = record['s3']['bucket']['name']
+#         object_key = record['s3']['object']['key']
+        
+#         try:
+#             # Detect labels in the image using Rekognition
+#             response = rekognition.detect_labels(
+#                 Image={
+#                     'S3Object': {
+#                         'Bucket': bucket_name,
+#                         'Name': object_key
+#                     }
+#                 },
+#                 MaxLabels=10
+#             )
+#             labels = [label['Name'] for label in response['Labels']]
+
+#             # Retrieve custom labels from S3 metadata
+#             response = s3.head_object(Bucket=bucket_name, Key=object_key)
+#             custom_labels = response['Metadata'].get('x-amz-meta-customlabels', '').split(',')
+
+#             # Merge Rekognition labels and custom labels
+#             all_labels = list(set(labels + custom_labels))
+
+#             # Prepare the document to be indexed in OpenSearch
+#             document = {
+#                 'objectKey': object_key,
+#                 'bucket': bucket_name,
+#                 'createdTimestamp': datetime.now().isoformat(),
+#                 'labels': all_labels
+#             }
+
+#             # Index the document in OpenSearch
+#             es.index(index='photos', doc_type='_doc', body=json.dumps(document))
+
+#         except ClientError as e:
+#             print(f'Error processing object {object_key} from bucket {bucket_name}. Error: {e}')
+
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Processing complete')
+#     }
---
+++ lambdas/LF2.py
@@ -0,0 +1,100 @@
+import boto3
+import json
+import os
+
+# check out LF0 for reference 
+
+lex = boto3.client('lexv2-runtime')
+opensearch = boto3.client('opensearchservice')
+
+def lambda_handler(event, context):
+    query_from_user = event['queryStringParameters']['q']
+
+    # Disambiguate the query using Amazon Lex
+    # lex_response = lex.post_text(
+    #     botName='YourLexBotName',
+    #     botAlias='',
+    #     userId='',
+    #     inputText=query_from_user
+    # )
+
+    lex_response = lex.recognize_text(
+        botId='WRDYKDEUOV',
+        botAliasId='SLVZYGVSIQ',
+        localeId='en_US',
+        sessionId='testuser',
+        text=query_from_user)
+
+    # Extract keywords from Lex response
+    keywords_from_lex = lex_response.get('slots', {}).values()
+
+    # Search in OpenSearch if keywords are present
+    if keywords_from_lex:
+        search_query = ' OR '.join(keywords_from_lex)
+        search_response = opensearch.search(
+            Index='photos',
+            Body={
+                'query': {
+                    'multi_match': {
+                        'query': search_query,
+                        'fields': ['labels']
+                    }
+                }
+            }
+        )
+        results = search_response['hits']['hits']
+    else:
+        results = []
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'results': results})
+    }
+
+
+# import boto3
+# import json
+# from botocore.exceptions import ClientError
+
+# def lambda_handler(event, context):
+#     lex = boto3.client('lex-runtime')
+#     es = boto3.client('es')
+
+#     query = event.get('queryStringParameters', {}).get('q', '')
+
+#     try:
+#         # Send the query to Lex for processing
+#         lex_response = lex.post_text(
+#             botName='YourBotName', # Replace with your bot name
+#             botAlias='YourBotAlias', # Replace with your bot alias
+#             userId='user-id', # A unique ID for the user
+#             inputText=query
+#         )
+
+#         # Extract keywords from Lex response (adapt based on your Lex setup)
+#         keywords = lex_response.get('slots', {}).values()
+
+#         # Search OpenSearch with the keywords
+#         search_query = ' '.join(keywords)
+#         es_response = es.search(
+#             index='photos',
+#             body={
+#                 'query': {
+#                     'match': {
+#                         'labels': search_query
+#                     }
+#                 }
+#             }
+#         )
+
+#         # Extract and format the search results
+#         results = [hit['_source'] for hit in es_response['hits']['hits']]
+
+#     except ClientError as e:
+#         print(f'Error: {e}')
+#         results = []
+
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps({'results': results})
+#     }
