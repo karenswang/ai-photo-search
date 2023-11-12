e2a6333c989d1a0e76b50f7e1d714cc66d8ca5ec
Karen Wang <46947530+karenswang@users.noreply.github.com>
2023-11-12 18:19:33 -0500

    update lf1

--- LF1.py
+++ LF1.py
@@ -3,15 +3,15 @@ import json
 import os
 from datetime import datetime
 from botocore.exceptions import ClientError
+import requests
 
 
 def lambda_handler(event, context):
     rekognition = boto3.client('rekognition')
     s3 = boto3.client('s3')
-    opensearch = boto3.client('opensearch') # es = boto3.client('es')
+    opensearch = boto3.client('es') # es = boto3.client('es')
     
     print("event: ", event)
-    print("context: ", context)
         
     # Get bucket name and object key from the S3 event
     bucket = event['Records'][0]['s3']['bucket']['name']
@@ -33,7 +33,8 @@ def lambda_handler(event, context):
 
     # Get custom labels from S3 metadata if available
     metadata = s3.head_object(Bucket=bucket, Key=object_key)
-    custom_labels = metadata.get('Metadata', {}).get('x-amz-meta-customlabels', '')
+    custom_labels = metadata.get('Metadata', {}).get('customlabels', '')
+    # print("custom_labels:", custom_labels)
     if custom_labels:
         labels.extend(custom_labels.split(','))
 
@@ -45,17 +46,45 @@ def lambda_handler(event, context):
         'labels': labels
     }
 
-    # Index the document in OpenSearch
-    index_response = opensearch.index(
-        Index='photos',
-        Body=json.dumps(document)
-    )
+    # # # Index the document in OpenSearch
+    # index_response = opensearch.index(
+    #     Index='photos',
+    #     Body=json.dumps(document)
+    # )
     # opensearch.index(index='photos', doc_type='_doc', body=document)
+    
+    
+    # Index the document in OpenSearch Service (Amazon Elasticsearch)
+    opensearch_endpoint = "https://search-photos-h5rkn7iyx5ntfplwz52f3bzf5q.us-east-1.es.amazonaws.com"
+    index_name = "photos"
+    
+    bulk_request_body = json.dumps({"index": {"_index": index_name}}) + "\n"
+    bulk_request_body += json.dumps(existing_document) + "\n"
+    
+    try:
+        # Send the bulk index request to OpenSearch
+        response = requests.post(
+            f"{opensearch_endpoint}/{index_name}/_bulk",
+            headers={"Content-Type": "application/json"},
+            data=bulk_request_body
+        )
+        
+    # Process the response
+        if response.status_code == 200:
+            result = json.loads(response.text)
+            print("Bulk indexing of existing document successful:", result)
+        else:
+            print("Bulk indexing of existing document failed with status code:", response.status_code)
+            print("Response content:", response.text)
+    except Exception as e:
+        print("An error occurred:", str(e))
+        # Handle the error as needed
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(index_response)
-    }
+
+    # return {
+    #     'statusCode': 200,
+    #     'body': json.dumps(index_response)
+    # }
 
 # def lambda_handler(event, context):
 #     for record in event['Records']:
